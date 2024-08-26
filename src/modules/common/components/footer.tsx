3a3e8263a5da7df0f5c680426926e56bbe68fdf0
Gajendra Chaturvedi <gchaturvedi@clarapth.com>
2024-08-26 17:17:44 +0530

    add screenshots

--- src/modules/common/components/footer.tsx
+++ src/modules/common/components/footer.tsx
@@ -16,7 +16,7 @@ export function Footer() {
           </Link>
           . The source code is available on{" "}
           <Link
-            href="https://github.com/salimi-my/shadcn-ui-sidebar"
+            href="https://github.com/gmchaturvedi1/nextjs-shadcn-dashboard"
             target="_blank"
             rel="noopener noreferrer"
             className="font-medium underline underline-offset-4"
