c0e4c518763cad40ba01fa92b3758ed5fbcc38ca
Gajendra Chaturvedi <gchaturvedi@clarapth.com>
2024-08-26 15:52:19 +0530

    Updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from "next";
+import dynamic from "next/dynamic";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const AdminPanelLayout = dynamic(
+  () => import("@/modules").then((module) => module.AdminPanelLayout),
+
+  {
+    ssr: false,
+  }
+);
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AdminPanelLayout>{children}</AdminPanelLayout>
+      </body>
     </html>
   );
 }
